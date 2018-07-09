0a2b03afe6fae8467ef060054d9f08b629c18be9
songjiangtao <songjiangtao@ciicsh.com.pst>
2018-07-09 18:00:26 +0800

    下班前

---
+++ readme.txt
@@ -0,0 +1,18 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++
+axios发送get post请求问题
+发送post请求时一般都要设置Content-Type，发送内容的类型，application/json指发送json对象但是要提前stringify一下。application/xxxx-form指发送？a=b&c=d格式，可以用qs的方法格式化一下，qs在安装axios后会自动安装，只需要组件里import一下即可。
+const postData=JSON.stringify(this.formCustomer);
+'Content-Type':'application/json'}
+
+const postData=Qs.stringify(this.formCustomer);//过滤成？&=格式
+'Content-Type':'application/xxxx-form'}
+
+axios({
+   url:'/api/xxxxx/xxxx.xxx',
+   method:'post',
+   data:postData,
+   headers: {
+     'Content-Type': 'application/x-www-form-urlencoded'
+   }
+})
++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- src/main.js
+++ src/main.js
@@ -3,20 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//import './assets/css/main.css'
 import axios from 'axios'
-import { Field, Button, Cell } from 'mint-ui'// 按需引入部分组件
-import { XButton, XInput } from 'vux'
+import { Field, Button} from 'mint-ui'// 按需引入部分组件
+Vue.component(Button.name, Button)
+Vue.component(Field.name, Field)
 
+import { XButton, XInput } from 'vux'
 Vue.component('x-button', XButton)
 Vue.component('x-input', XInput)
-
-Vue.component(Field.name, Field)
-Vue.component(Button.name, Button)
-Vue.component(Cell.name, Cell)
 
 Vue.config.productionTip = false
 // 开启debug模式
 Vue.config.debug = true
+
 // require('./assets/css/base.css');// 引入全局的base文件
 axios.defaults.withCredentials = true
 Vue.prototype.$axios = axios// 其他页面在使用axios的时候直接  this.$axios就可以了
