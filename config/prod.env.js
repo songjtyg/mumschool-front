f128c650986012f8fe15fba99d0bad93a2c61919
songjiangtao <songjiangtao@ciicsh.com.pst>
2018-07-10 17:38:45 +0800

    周二在上海代码完善

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  BACKSTAGE_HOST:'"http://localhost:80"'
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// require('./assets/css/reset.css');// 引入全局的文件
+// import './assets/css/reset.css'
 import Vue from 'vue'
+Vue.config.productionTip = false //阻止 vue 在启动时生成生产提示
+Vue.config.debug = true // 开启debug模式
+
 import App from './App'
 import router from './router'
-//import './assets/css/main.css'
+
 import axios from 'axios'
+axios.defaults.withCredentials = true // 缺省在调用时带上session
+Vue.prototype.$axios = axios// 其他页面在使用axios的时候直接  this.$axios就可以了
+
 import { Field, Button} from 'mint-ui'// 按需引入部分组件
 Vue.component(Button.name, Button)
 Vue.component(Field.name, Field)
@@ -13,13 +21,6 @@ import { XButton, XInput } from 'vux'
 Vue.component('x-button', XButton)
 Vue.component('x-input', XInput)
 
-Vue.config.productionTip = false
-// 开启debug模式
-Vue.config.debug = true
-
-// require('./assets/css/base.css');// 引入全局的base文件
-axios.defaults.withCredentials = true
-Vue.prototype.$axios = axios// 其他页面在使用axios的时候直接  this.$axios就可以了
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
