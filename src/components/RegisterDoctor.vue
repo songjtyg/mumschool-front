1c1537d1049e3e8c54bd3726c25aabf209b0be70
songjiangtao <songjtyg@sina.com>
2018-07-12 00:49:42 +0800

    调整医生注册页面

--- src/components/RegisterDoctor.vue
+++ src/components/RegisterDoctor.vue
@@ -1,21 +1,23 @@
 <template xmlns:style="http://www.w3.org/1999/xhtml">
-  <div flex="dir:top main:top cross:center " style="width:100%; height: 42rem; ">
-    <div style="width:80%; height: 28rem; background: white;opacity: 1;" >
+  <div flex="dir:top main:center cross:center " style="width:100%; height: 36rem; ">
+    <div style="width:85%; height: 25rem; background: white;opacity: 1;" >
       <group title="">
-        <x-input title="用户名/手机" placeholder="请输入用户名/手机" mask="999 9999 9999" v-model="maskValue" :max="13" is-type="china-mobile" style="height: 2rem;"></x-input>
-        <x-input title="密码" placeholder="请输入密码" type="password" v-model="password" :min="6" :max="6" @on-change="change" style="height: 2rem;"></x-input>
         <x-input title="姓名" name="username" placeholder="请输入姓名" is-type="china-name"></x-input>
+        <x-input title="密码" placeholder="请输入密码" type="password" v-model="password" :min="6" :max="6" @on-change="change" style="height: 2rem;"></x-input>
         <x-input title="单位" placeholder="" :min="2" :max="5"></x-input>
         <x-input title="科室" placeholder="" :min="2" :max="5"></x-input>
         <x-input title="手机号码" name="mobile" placeholder="请输入手机号码" keyboard="number" is-type="china-mobile"></x-input>
-        <x-input title="发送验证码" class="weui-vcode">
-          <x-button slot="right" type="primary" mini>发送验证码</x-button>
+        <x-input title="验证码" class="weui-vcode">
+          <x-button slot="right" type="primary" mini style="width:9rem">发送验证码</x-button>
         </x-input>
       </group>
     </div>
-    <div flex="dir:left main:left cross:center "  style="width:80%; height: 1rem; margin-bottom: 1rem;">
+    <div flex="dir:left main:left cross:center "  style="width:85%; height: 0.5rem; margin-bottom: 0.5rem;">
     </div>
-    <x-button type="primary" style="width:80%; margin-bottom: 1rem;">提交</x-button>
+    <x-button type="primary" style="width:85%; margin-bottom: 1rem;">提交</x-button>
+    <div flex="dir:left main:left cross:center "  style="width:85%; height: 1rem; margin-bottom: 1rem;">
+      <a style="color:white;">*注册孕婴宝俱乐部，代表您已阅读并同意隐私条款</a>
+    </div>
   </div>
 </template>
 
