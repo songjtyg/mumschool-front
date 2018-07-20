47c68bc9f93a7c3d2c6ad9685f0ec0e7a98f7ce8
songjiangtao <songjiangtao@ciicsh.com.pst>
2018-07-20 19:14:25 +0800

    修改键盘挡住输入框bug-在上海

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,20 +1,24 @@
 <template xmlns:style="http://www.w3.org/1999/xhtml">
   <div flex="dir:top main:center cross:center " class="page-wrap-normal" id="aaa">
+    <div style="height:5rem;">daf</div>
       <div style="width:85%; height: 10rem; background: white;opacity: 1;" >
         <!--<mt-field label="登录" placeholder="用户名/手机" v-model="username"></mt-field>-->
         <!--<mt-field label="密码" placeholder="请输入密码" type="password" v-modal="password"></mt-field>-->
+
         <group title="">
-          <x-input  title="用户名/手机" placeholder="请输入用户名/手机" v-model="loginWord" :max="13" style="height: 2rem;" @click.native="isAndroid"></x-input>
-          <x-input id="asd" title="密码" placeholder="请输入密码" type="password" v-model="password" :min="6" :max="6" @on-change="change" @click="isAndroid" style="height: 2rem;"></x-input>
+          <x-input  title="用户名/手机" placeholder="请输入用户名/手机" v-model="loginWord" :max="13" style="height: 2rem;" @click="isAndroid"></x-input>
+          <x-input  title="密码" placeholder="请输入密码" type="password" v-model="password" :min="6" :max="6" @on-change="change" @click.native="isAndroid" style="height: 2rem;"></x-input>
         </group>
   </div>
+
     <div flex="dir:left main:right cross:center "  style="width:85%; height: 2rem; margin-bottom: 1rem;">
       <!--<mt-cell title="标题文字" width="0.5rem" height="0.5rem">-->
         <img slot="icon" src="../assets/images/lock.png"  style="width:1.5rem;height:1.5rem"/><a style="color:white;">忘记密码</a>
       <!--</mt-cell>-->
     </div>
       <x-button type="primary" :disabled="disabledClickLoginButton" @click.native="login" style="width:85%; margin-bottom: 1rem;">登录</x-button>
-      <x-button type="primary"  @click.native="toRegister" style="width:85%; margin-bottom: 1rem;">新用户注册</x-button>
+      <x-button id="asd" type="primary"  @click.native="toRegister" style="width:85%; margin-bottom: 1rem;">新用户注册</x-button>
+    <input type="text" placeholder="Type here..."></input>
   </div>
 </template>
 
@@ -92,14 +96,20 @@
         this.$router.push({name: 'Register'})
       },
 
-      isAndroid () {
-        document.getElementById("asd").scrollIntoView(true);
-//        var target = this;
-//        setTimeout(function(){
-//          aler(1)
-//          target.scrollIntoViewIfNeeded();
-//          //alert('scrollIntoViewIfNeeded');
-//        },400);
+      isAndroid (event) {
+        var that = this;
+        var target = document.getElementById("asd")//.scrollIntoView(true);
+        //var target = event.currentTarget;
+        //var target = this;
+        setTimeout(function(){
+          that.loginWord = "123"
+          //target.scrollIntoView(false);
+          //target.scrollIntoViewIfNeeded();
+          target.scrollIntoView(true);
+          //window.scrollTo(0,target.scrollHeight);
+          //document.activeElement.scrollIntoViewIfNeeded();
+          that.loginWord = "456"
+        },400);
       }
     },
     mounted() {
@@ -122,7 +132,7 @@
     },
     //created() {
     beforeCreate() {
-      debugger
+      debugger;
       var that = this;
       this.$axios.get(`http://mumschool.ngrok.xiaomiqiu.cn/weixinUser/ifOpenid`).then(function(response) {
         let hasOpenid = response.data;
